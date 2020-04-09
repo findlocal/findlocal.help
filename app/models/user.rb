0179cea007213cb774816d2dec38dd446370bb99
jones-sam <samjones6058@gmail.com>
2020-04-09 15:10:40 -0300

    created task and user references

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :task_applications
+  has_many :creators, class_name: 'Task', foreign_key: 'creator_id'
+  has_many :helpers, class_name: 'Task', foreign_key: 'helper_id'
 end
