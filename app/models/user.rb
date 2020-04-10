d2c34cae0ef360e3b5742c98a70b78c66edfd70f
peter westman <pwestman@gmail.com>
2020-04-10 17:40:55 +0200

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :first_name, :last_name, length: { minimum: 2, maximum: 20 }, presence: true
+  validates :email, format: { with: ^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$ } #expression matches email addresses, and checks that they are of the proper form. It checks to ensure the top level domain is between 2 and 4 characters long, but does not check the specific domain against a list
+  validates :password, :address, presence: true #do we want to have a max password length?
+  validates :phone_number, format: {with :  ^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$ }#regex for number with international dialing code
+
+
+
   has_many :task_applications
   has_many :tasks, through: :task_applications
   has_many :creators, class_name: 'Task', foreign_key: 'creator_id'
