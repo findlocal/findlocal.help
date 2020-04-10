a227bb585ad8f1bd854498815e175195c5fec367
jones-sam <samjones6058@gmail.com>
2020-04-10 09:59:46 -0300

    deleted not needed migration

--- db/migrate/20200409160053_add_references_to_users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddReferencesToUsers < ActiveRecord::Migration[6.0]
-  def change
-    # add_reference :users, :task_application, foreign_key: true
-    # add_reference :users, :task
-  end
-end
