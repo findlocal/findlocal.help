a2b68d645157c0c091bb93c4c3600c19c3bc7838
peter westman <pwestman@gmail.com>
2020-04-10 18:06:24 +0200

    task and user

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,7 @@ class Task < ApplicationRecord
   belongs_to :helper, class_name: 'User'
   has_many :task_tags
   has_many :tags, through: :task_tags
+
+  validates :title, :description, :due_date, :location, :status, presence: true
+  validates_associated :task_tags, :tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :email, format: { with: ^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$ } #expression matches email addresses, and checks that they are of the proper form. It checks to ensure the top level domain is between 2 and 4 characters long, but does not check the specific domain against a list
   validates :password, :address, presence: true #do we want to have a max password length?
   validates :phone_number, format: {with :  ^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$ }#regex for number with international dialing code
-
+  validates_associated :task_applications, :tasks, :creators, :helpers
 
 
   has_many :task_applications
