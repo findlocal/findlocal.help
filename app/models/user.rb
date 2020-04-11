a26786945b1266be3b134962a8e9eae2cc8b9aab
Gabriele Canepa <git@gabrielecanepa.com>
2020-04-11 13:56:32 +0200

    Fix regex for phone and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,35 +1,30 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # Constants at the top:
+  PHONE_REGEX = /\A(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*\z/
 
-  EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
-  PHONE_REGEX = /^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$/
+  # Devise:
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  validates :first_name, :last_name, length: { minimum: 2, maximum: 20 }, presence: true
-  validates :email, format: { with: EMAIL_REGEX, message: "is not a valid email" }
-  validates :password, presence: true
-  validates :address, presence: true
-  validates :phone_number, format: { with: PHONE_REGEX, message: "is not a valid phone number" }
-  validates_associated :task_applications, :tasks, :creators, :helpers
-
-
+  # Associations:
   has_many :task_applications
   has_many :tasks, through: :task_applications
   has_many :creators, class_name: 'Task', foreign_key: 'creator_id'
   has_many :helpers, class_name: 'Task', foreign_key: 'helper_id'
   has_one_attached :avatar
 
-  def applications # alias
+  # Validations:
+  validates :first_name, :last_name, length: { minimum: 2, maximum: 20 }, presence: true
+  # use the regex from devise (it's just an helper the gem is giving us):
+  validates :email, format: { with: Devise::email_regexp, message: "is not a valid email" }
+  validates :phone_number, format: { with: PHONE_REGEX, message: "is not a valid phone number" }
+  validates_associated :task_applications, :tasks, :creators, :helpers
+
+  # Custom methods and aliases:
+  def applications
     task_applications
   end
 
-  def accepted_applications
-    # find all tasks where the current user was picked to be the helper
-    # (chances are in the current seed script there will be none)
-    tasks.where(helper: id)
+  def accepted_applications # find all tasks where the current user was picked to be the helper
+    tasks.where(helper: self)
   end
 end
