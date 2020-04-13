acab6b9f1d2fc591d5b8846fb82a17cb429bddfd
peter westman <pwestman@gmail.com>
2020-04-13 16:54:56 +0200

    update tasks

--- app/policies/tasks_policy.rb
+++ app/policies/tasks_policy.rb
@@ -5,11 +5,14 @@ class TasksPolicy < ApplicationPolicy
 	end
 
 	def create
+		user?
   end
 
   def update?
       user.creator? #only permit the creator of the task to update the task
   end
+
+
 
 
 end
