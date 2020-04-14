f04583693399221ddc85e7aca6c0dd4a07910e80
Molly Bollig <mollybollig@gmail.com>
2020-04-14 18:34:47 +0200

    tmpcommit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,5 @@ require('channels')
 // Packages
 import 'bootstrap'
 
-// Required below for the datetimepicker - to be left commented per instructions
-//= require moment
+//= require bootstrap-datepicker // for js
 
-//= require bootstrap-datetimepicker
