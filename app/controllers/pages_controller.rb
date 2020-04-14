fe633d49cd03e47f8bceb86fb3226838603c273a
Savina Aliaj savina.aliaj@gmail.com <savina.aliaj@gmail.com>
2020-04-14 12:48:43 +0200

    created dashboard routes and basic view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def dashboard
+    @created_tasks = current_user.created_tasks
+    @helper_tasks = current_user.helper_tasks
+  end
 end
