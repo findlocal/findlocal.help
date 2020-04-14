d3642c96c35eec657b02a89930ab5803084737ce
jones-sam <samjones6058@gmail.com>
2020-04-14 12:57:10 -0300

    finally fixed the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+require 'open-uri'
+
+# this prevents URI.open from creating STRINGIO's for small files, which does not work with activestorage
+OpenURI::Buffer.send :remove_const, 'StringMax' if OpenURI::Buffer.const_defined?('StringMax')
+OpenURI::Buffer.const_set 'StringMax', 0
+
 puts "Destroying all records..."
 User.destroy_all
 Task.destroy_all
@@ -6,8 +12,9 @@ TaskTag.destroy_all
 Tag.destroy_all
 
 puts "Creating new users..."
+BASE_URL = "https://randomuser.me/api/portraits/men/"
 30.times do |n|
-  User.create(
+  user = User.create(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     email: "user#{n + 1}@localhelp.org", # generate emails user1@localhelp.org, user2@localhelp.org, etc.
@@ -15,6 +22,13 @@ puts "Creating new users..."
     address: Faker::Address.full_address,
     phone_number: Faker::PhoneNumber.cell_phone
   )
+
+  file = URI.open("#{BASE_URL}#{n + 1}.jpg")
+  puts "Creating User Avatar..."
+
+  user.avatar.attach(io: file, filename: 'avatar.jpg', content_type: 'image/jpeg')
+  user.save
+
 end
 
 puts "Creating tags..."
@@ -35,6 +49,13 @@ puts "Creating tasks with helps..."
     creator: User.all.sample,
     helper: [User.all.sample, nil].sample # could be not assigned yet, so let's do 50% of the times
   )
+
+  rand(1..3).times do |n|
+  puts "Creating Task Photo..."
+
+    file = URI.open("https://picsum.photos/500")
+    task.photos.attach(io: file, filename: "photo#{n + 1}", content_type: 'image/jpeg')
+  end
 
   rand(1..10).times do
     TaskTag.create(
