0239a59e2973a712688e7f557cbf4638686bd218
Molly Bollig <mollybollig@gmail.com>
2020-04-15 17:30:34 +0200

    removed logic from pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,5 @@
 class PagesController < ApplicationController
   def home
-    if params[:query].present?
-      @tasks = Task.where("location ILIKE ?", "%#{params[:query]}%")
-    else
-      @tasks = Task.all
-    end  
   end
 
   def dashboard
