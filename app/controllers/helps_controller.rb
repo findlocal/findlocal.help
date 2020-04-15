704b664bb018d7243480635aaba8363aa8b35ad1
Savina Aliaj savina.aliaj@gmail.com <savina.aliaj@gmail.com>
2020-04-15 17:36:44 +0200

    Minor fixes

--- app/controllers/helps_controller.rb
+++ app/controllers/helps_controller.rb
@@ -1,16 +1,13 @@
 class HelpsController < ApplicationController
   def create
     @task = Task.find(params[:task_id])
-
-    @help = Help.new()
-    @help.user = current_user
-    @help.task = @task
+    @help = Help.new(user: current_user, task: @task)
     if @help.save
       flash[:alert] = "Applied!"
-      redirect_to tasks_path
     else
       flash[:alert] = "Application did not go through"
     end
+    redirect_to tasks_path
   end
 
   def update
@@ -23,11 +20,4 @@ class HelpsController < ApplicationController
     flash[:alert] = "Help successfully deleted."
     redirect_to dashboard_path
   end
-
-  private
-
-  # def help_params
-  #   params.require(:help).permit()
-
-  # end
 end
