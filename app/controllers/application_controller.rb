80f0a2be4c64ea6438292e30fe6cebe1f1074fce
Savina Aliaj savina.aliaj@gmail.com <savina.aliaj@gmail.com>
2020-04-15 17:36:44 +0200

    fixed sign up and added avatar palceholder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :address, :phone_number, :email, :password, :current_password) }
   end
 end
