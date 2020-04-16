2d6b57db8c1fbf01b8bfb853a47e31698a05fadb
jones-sam <samjones6058@gmail.com>
2020-04-16 11:58:12 -0300

    able to view and accept/decline applications

--- app/controllers/helps_controller.rb
+++ app/controllers/helps_controller.rb
@@ -4,17 +4,8 @@ class HelpsController < ApplicationController
     @help = Help.new(help_params)
     @help.user = current_user
     @help.task = @task
-<<<<<<< HEAD
     flash[:alert] = @help.save ? "Applied!" : "Application did not go through"
     redirect_to dashboard_path
-=======
-    if @help.save
-      flash[:alert] = "Applied!"
-    else
-      flash[:alert] = "Application did not go through"
-    end
-    redirect_to tasks_path
->>>>>>> implemented ability to add a message to your help
   end
 
   def update
