a0c0ef2506f50a7e91eebd744dac414282bf8ce8
jones-sam <samjones6058@gmail.com>
2020-04-21 11:32:43 -0300

    created review models and connections

---
+++ db/migrate/20200421142458_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reviews do |t|
+      t.string :rating
+      t.references :task, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20200421142750_create_review_fields.rb
@@ -0,0 +1,12 @@
+class CreateReviewFields < ActiveRecord::Migration[6.0]
+  def change
+    create_table :review_fields do |t|
+      t.string :name
+      t.text :content
+      t.string :rating
+      t.references :review, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
