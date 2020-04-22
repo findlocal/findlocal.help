e57cea6576088900ccb017ee07dccb42fa11d56d
Molly Bollig <mollybollig@gmail.com>
2020-04-22 21:53:43 +0200

    tmp commit

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,9 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-localhelp');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= 100) {
+        // if (window.scrollY >= window.innerHeight) {
+
         navbar.classList.add('navbar-localhelp-white');
       } else {
         navbar.classList.remove('navbar-localhelp-white');
