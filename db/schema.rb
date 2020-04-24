241a1251fb61337ee5f447f3ee8c7450df808e86
peter westman <pwestman@gmail.com>
2020-04-24 16:55:13 +0200

    debugging

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_134335) do
+ActiveRecord::Schema.define(version: 2020_04_24_134256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,7 +91,6 @@ ActiveRecord::Schema.define(version: 2020_04_24_134335) do
     t.bigint "helper_id"
     t.float "latitude"
     t.float "longitude"
-    t.integer "postcode"
     t.index ["creator_id"], name: "index_tasks_on_creator_id"
     t.index ["helper_id"], name: "index_tasks_on_helper_id"
   end
