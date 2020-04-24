eec55547c235e796a5557559210724abc5421a97
Gabriele Canepa <git@gabrielecanepa.com>
2020-04-24 17:50:45 +0200

    Minor fixes

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,22 +1,15 @@
 Geocoder.configure(
-  # Geocoding options
+  lookup: :nominatim,           # name of geocoding service (symbol)
+  units: :km,                   # :km for kilometers or :mi for miles
   # timeout: 3,                 # geocoding service timeout (secs)
-  lookup: :nominatim,        # name of geocoding service (symbol)
   # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  #api_key: :ENV['ALGOLIA_API'],            # API key for geocoding service
+  # api_key: nil,               # API key for other geocoding services
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
-
-  # Exceptions that should not be rescued by default
-  # (if you want to implement custom error handling);
-  # supports SocketError and Timeout::Error
-  # always_raise: [],
-
-  # Calculation options
-  units: :km,                # :km for kilometers or :mi for miles
+  # always_raise: [],           # exceptions that should not be rescued by default
   # distances: :linear          # :spherical or :linear
-  )
+)
