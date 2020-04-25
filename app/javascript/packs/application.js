5dde1b217ef8d671cf2079eb6091151d734ba079
peter westman <pwestman@gmail.com>
2020-04-25 12:19:01 +0200

    places

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,5 +32,18 @@ document.addEventListener("turbolinks:load", () => {
   updateNavbarWhiteOnScroll()
 })
 
+//const algoliaPlaces = function() {
+//const for_input = document.querySelector('#task_location')
+// var places = require('places.js');
+// var placesAutocomplete = places({
+//   appId: 'plQV4XVJ2NEK',
+//   apiKey: '1e8728c06104453bdd9931929be08b81',
+//   container: document.querySelector('#task_location')
+// });
+// }
+// const test = document.querySelector('#task_location')
+// if (test) { 
+// 	algoliaPlaces()
+// }
 algoliaPlaces()
 autocompleteSearch()
