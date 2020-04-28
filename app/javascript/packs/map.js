b716b7f2e690856d5313ff2bd9b656668f980db9
peter westman <pwestman@gmail.com>
2020-04-28 19:37:23 +0200

    cleaning up

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -12,17 +12,3 @@ const mapElement = document.getElementById('map');
  };
 
   export { initMapbox };
-
-//  // const addMarkersToMap = (map, markers) => {
-//   markers.forEach((marker) => {
-//     new mapboxgl.Marker()
-//       .setLngLat([ marker.lng, marker.lat ])
-//       .addTo(map);
-//   });
-// };
-
-// const fitMapToMarkers = (map, markers) => {
-//   const bounds = new mapboxgl.LngLatBounds();
-//   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-//   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
-// };
