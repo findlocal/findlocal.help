83d83c3729f59dd2ed589f019a2dd32718f3ee23
peter westman <pwestman@gmail.com>
2020-04-28 18:50:50 +0200

    geocode

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -2,7 +2,6 @@
 
 const initMapbox = () => {
 const mapElement = document.getElementById('map');
-console.log("Hello Map")
   if (mapElement) { // only build a map if there's a div#map to inject into
      mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
