0c37cb4a4fa31455a1c944a98b001d13239fd674
jones-sam <samjones6058@gmail.com>
2020-05-01 12:25:41 -0300

    added stats to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,17 @@
 class UsersController < ApplicationController
   before_action :set_and_authorize_task, only: [:show, :index]
 
-  def index 
-    @users = User.all 
+  def index
+    @users = User.all
   end
 
-    
   def show
     @user = User.find(params[:id])
-    @users = User.all 
+    @users = User.all
+
+    @completed = Task.where(helper: @user).where(status: "completed").count
+    @in_progress = Task.where(helper: @user).where(status: "in progress").count
+    @total = @completed + @in_progress
   end
 end
 
@@ -20,6 +23,5 @@ def set_and_authorize_task
 end
 
 def user_params
-  params.require(:user).permit(:email, :first_name, :last_name, :address, :phone_number )
+  params.require(:user).permit(:email, :first_name, :last_name, :address, :phone_number)
 end
-
