25faef39bc2bf2e04e04ab2d6f4529318c9d18eb
jones-sam <samjones6058@gmail.com>
2020-05-01 11:51:02 -0300

    added bio to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_140745) do
+ActiveRecord::Schema.define(version: 2020_05_01_144943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 2020_04_26_140745) do
     t.string "address"
     t.string "phone_number"
     t.string "stripe_account"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
