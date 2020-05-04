fa0413b96e13c1c251ca64ecf6c8b799a28ae849
jones-sam <samjones6058@gmail.com>
2020-05-04 15:35:38 -0300

    commneting out console logs

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,23 +1,25 @@
-import 'js-autocomplete/auto-complete.css';
-import autocomplete from 'js-autocomplete';
-const autocompleteSearch = function() {
-  const s = document.getElementById('search-data')
-  console.log(s)
-  const skills = JSON.parse(document.getElementById('search-data').dataset.skills)
-  const searchInput = document.getElementById('search_');
+import "js-autocomplete/auto-complete.css"
+import autocomplete from "js-autocomplete"
+const autocompleteSearch = function () {
+  const s = document.getElementById("search-data")
+  // console.log(s)
+  const skills = JSON.parse(
+    document.getElementById("search-data").dataset.skills
+  )
+  const searchInput = document.getElementById("search_")
   if (skills && searchInput) {
     new autocomplete({
       selector: searchInput,
       minChars: 1,
-      source: function(term, suggest){
-          term = term.toLowerCase();
-          const choices = skills;
-          const matches = [];
-          for (let i = 0; i < choices.length; i++)
-              if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
-          suggest(matches);
+      source: function (term, suggest) {
+        term = term.toLowerCase()
+        const choices = skills
+        const matches = []
+        for (let i = 0; i < choices.length; i++)
+          if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i])
+        suggest(matches)
       },
-    });
+    })
   }
-};
-export { autocompleteSearch };
+}
+export { autocompleteSearch }
