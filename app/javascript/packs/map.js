b30e9940b56814e786dc20f73fd73ccbd3fcfe03
jones-sam <samjones6058@gmail.com>
2020-05-06 08:09:51 -0300

    fixed markers

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,46 +1,46 @@
-const mapElement = document.getElementById('map');
+const mapElement = document.getElementById("map")
 
 const buildMap = () => {
-  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey
   return new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
     center: [9.194691, 45.471711],
-    zoom: 22
-  });
-};
+    zoom: 22,
+  })
+}
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
-  const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow)
 
-  const element = document.createElement('div');
-  element.className = 'marker';
-  element.style.backgroundSize = 'contain';
-  element.style.width = '25px';
-  element.style.height = '25px';
- 
+    const element = document.createElement("div")
+    element.className = "marker"
+    element.style.backgroundSize = "contain"
+    element.style.width = "25px"
+    element.style.height = "25px"
+
     new mapboxgl.Marker(element)
-      .setLngLat([ marker.lng, marker.lat ])
+      .setLngLat([marker.lng, marker.lat])
       .setPopup(popup)
-      .addTo(map);
-  });
-};
+      .addTo(map)
+  })
+}
 
 const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
-};
+  const bounds = new mapboxgl.LngLatBounds()
+  markers.forEach((marker) => bounds.extend([marker.lng, marker.lat]))
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15 })
+}
 
 const initMapbox = () => {
   if (mapElement) {
-    const map = buildMap(); 
-    console.log(mapElement);
-    const markers = JSON.parse(mapElement.dataset.markers);
-    addMarkersToMap(map, markers);
-    fitMapToMarkers(map, markers);
+    const map = buildMap()
+    console.log(mapElement)
+    const markers = JSON.parse(mapElement.dataset.markers)
+    addMarkersToMap(map, markers)
+    fitMapToMarkers(map, markers)
   }
-};
+}
 
-export { initMapbox };
+export { initMapbox }
